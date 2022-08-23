56c825f152f363e83a2d451fc18a7ce79bf80d33
heejin <dongdong7251@naver.com>
2022-08-23 23:29:54 +0900

    add: camera move

--- LUNO/Assets/Scripts/Scene10_Desert/SaraAction.cs
+++ LUNO/Assets/Scripts/Scene10_Desert/SaraAction.cs
@@ -7,12 +7,17 @@ public class SaraAction : MonoBehaviour
     [SerializeField]
     private PrologueManager prologueManager;
     public CameraController Camera;
+    public Camera cam;
+    //float smoothTime = 10f;    //목표도달까지 걸리는 시간
+    //Vector3 moveVelocity = new Vector3(2.0f, 0, 0);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Sara"))
         {
-
+            Debug.Log("trigger");
+            Debug.Log(cam.transform.position);
+            StartCoroutine(cameraMove());
             collision.gameObject.GetComponent<Animator>().SetBool("isWakeUp", true);
             StartCoroutine(Popup());
         }
@@ -24,8 +29,15 @@ public class SaraAction : MonoBehaviour
         prologueManager.StartPrologue();
     }
 
-   /* IEnumerator cameraMove()
+    IEnumerator cameraMove()
     {
-        Camera.Camera
-    }*/
+        Camera.cameraMove = false;
+        Debug.Log(cam.transform.localPosition);
+        Vector3 targetPosition = new Vector3(cam.transform.localPosition.x + 15000000000000, cam.transform.localPosition.y, cam.transform.localPosition.z);
+        cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, targetPosition,
+                                               0.0000000000005f);
+        Debug.Log(cam.transform.localPosition);
+        yield return new WaitForSeconds(2f);
+
+    }
 }
