c7c1936e1aba89d940cbf7008aacdf560eb2336c
YoungEun <y_e_99@ewhain.net>
2022-08-24 18:12:31 +0900

    fix: camera move

--- LUNO/Assets/Scripts/Scene2_LibaryDoor/DoorInteraction.cs
+++ LUNO/Assets/Scripts/Scene2_LibaryDoor/DoorInteraction.cs
@@ -5,30 +5,43 @@ using UnityEngine;
 public class DoorInteraction : MonoBehaviour
 {
     public GameObject door;
-    public CameraController cam;
+    public GameObject doorNoKey;
+    public Camera cam;
+    CameraController cameraController;
     public Scene2_LibraryDoor_UIManager UIManager;
     Animator detailAnim;
     bool infoOnSwitch = false;
     bool doorAct = false;
 
+    [SerializeField]
+    Vector3 doorPosition;
+
     void Start()
     {
         detailAnim = door.GetComponent<Animator>();
-
+        cameraController = cam.GetComponent<CameraController>();
     }
     void Update()
     {
         if(!door.activeSelf && infoOnSwitch==true && Input.GetKeyDown(KeyCode.E))
         {
+            transform.position = doorPosition;  //지정된 위치로 이동
             door.SetActive(true);
-            //cam.DetailOn();
+
+            //카메라 이동
+            cameraController.targetPosition
+                = new Vector3(cam.transform.position.x + 2.5f, cam.transform.position.y, cam.transform.position.z);
+            cameraController.smoothTime = 0.38f;
+            cameraController.cameraMove = false;
+            cameraController.moveRight = true;
+
             doorAct = true;
             infoOnSwitch = false;
         }
         else if(door.activeSelf && Input.GetKeyDown(KeyCode.E))
         {
             UIManager.hasItem = true;
-            //cam.DetailOff();
+
             detailAnim.SetTrigger("PressE");
             StartCoroutine(DisappearDelay());
         }
