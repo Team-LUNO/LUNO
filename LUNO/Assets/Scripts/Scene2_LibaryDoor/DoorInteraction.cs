0d68af8c086ccab37e100d901f9a58a71085a354
YoungEun <y_e_99@ewhain.net>
2022-08-24 19:57:09 +0900

    fix: door interaction

--- LUNO/Assets/Scripts/Scene2_LibaryDoor/DoorInteraction.cs
+++ LUNO/Assets/Scripts/Scene2_LibaryDoor/DoorInteraction.cs
@@ -4,68 +4,42 @@ using UnityEngine;
 
 public class DoorInteraction : MonoBehaviour
 {
-    public GameObject door;
-    public GameObject doorNoKey;
-    public Camera cam;
-    CameraController cameraController;
     public Scene2_LibraryDoor_UIManager UIManager;
-    Animator detailAnim;
-    bool infoOnSwitch = false;
-    bool doorAct = false;
-
-    [SerializeField]
-    Vector3 doorPosition;
 
     void Start()
     {
-        detailAnim = door.GetComponent<Animator>();
-        cameraController = cam.GetComponent<CameraController>();
+        
     }
     void Update()
     {
-        if(!door.activeSelf && infoOnSwitch==true && Input.GetKeyDown(KeyCode.E))
-        {
-            transform.position = doorPosition;  //지정된 위치로 이동
-            door.SetActive(true);
-
-            //카메라 이동
-            cameraController.targetPosition
-                = new Vector3(cam.transform.position.x + 2.5f, cam.transform.position.y, cam.transform.position.z);
-            cameraController.smoothTime = 0.38f;
-            cameraController.cameraMove = false;
-            cameraController.moveRight = true;
-
-            doorAct = true;
-            infoOnSwitch = false;
-        }
-        else if(door.activeSelf && Input.GetKeyDown(KeyCode.E))
-        {
-            UIManager.hasItem = true;
-
-            detailAnim.SetTrigger("PressE");
-            StartCoroutine(DisappearDelay());
-        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!doorAct && collision.gameObject.CompareTag("LibraryDoor"))
+        if (!UIManager.doorAct && collision.gameObject.CompareTag("LibraryDoor"))
         {
-            infoOnSwitch = true;
+            UIManager.doorOnSwitch = true;
+        }
+
+        if(UIManager.hasItem && collision.gameObject.CompareTag("LibraryDoor"))
+        {
+            UIManager.keyOnSwitch = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(!doorAct && collision.gameObject.CompareTag("LibraryDoor"))
+        if(!UIManager.doorAct && collision.gameObject.CompareTag("LibraryDoor"))
         {
-            infoOnSwitch = false;
+            UIManager.doorOnSwitch = false;
+        }
+
+        if (UIManager.hasItem && collision.gameObject.CompareTag("LibraryDoor"))
+        {
+            UIManager.keyOnSwitch = false;
         }
     }
 
-    IEnumerator DisappearDelay()
-    {
-        yield return new WaitForSecondsRealtime(1f);
-        door.SetActive(false);
-    }
+    
 }
