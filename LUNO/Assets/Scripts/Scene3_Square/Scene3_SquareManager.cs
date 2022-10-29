6692c1dcd327d548a39bc3e2f59f46c2ddec5bd1
YoungEun <y_e_99@ewhain.net>
2022-10-29 19:46:31 +0900

    add: Scene2 timeline & fix:UI Manager

--- LUNO/Assets/Scripts/Scene3_Square/Scene3_SquareManager.cs
+++ LUNO/Assets/Scripts/Scene3_Square/Scene3_SquareManager.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class Scene3_SquareManager : MonoBehaviour
 {
+    public int sceneNum;
+
+    [SerializeField]
+    private PlayableDirector[] director;
+
     [SerializeField]
     private GameObject player;
+
+    [SerializeField]
+    private Vector3[] startPosition;
+
+    [SerializeField]
+    private GameObject[] InfoUI;
+
+    [SerializeField]
+    private bool infoUISwitch;
 
     [SerializeField]
     private GameObject oldDog;
@@ -18,11 +33,15 @@ public class Scene3_SquareManager : MonoBehaviour
     CameraController cameraController;
     public GameObject[] bubble;
     public GameObject[] item;
+    private float noMoveTime = 0f;
 
-    //only one item
-    bool hasItem;
-    string hasItemName;
-    string getItemName;
+    [SerializeField]
+    private GameObject graveyardBubble;
+
+    [SerializeField]
+    private GameObject flowerBubble;
+
+    int benchDialogue = 0;
 
     //library
     [SerializeField]
@@ -34,7 +53,7 @@ public class Scene3_SquareManager : MonoBehaviour
     [SerializeField]
     Vector3 libraryPosition;
 
-    Animator detailAnim;
+    Animator animator;
 
     //dialogue
     [SerializeField]
@@ -42,145 +61,259 @@ public class Scene3_SquareManager : MonoBehaviour
 
     //scene
     public GameObject blackScreen;
-    Animator blackScreenAnim;
-
-    int sceneNum;
 
     void Start()
     {
         cameraController = cam.GetComponent<CameraController>();
-        blackScreenAnim = blackScreen.GetComponent<Animator>();  
-        
-        /*
-        if(quest1 == 1 && !enterLibrary)
+        animator = blackScreen.GetComponent<Animator>();
+
+        if (sceneNum == 2 && !infoUISwitch)
         {
-            oldDog.SetActive(true);
-            libraryOn = true;
+            player.transform.position = startPosition[0];
+            director[0].Play();
+            infoUISwitch = true;
         }
-        */
+        else if (sceneNum == 2 && infoUISwitch)
+        {
+            director[1].Play();
+            player.transform.position = startPosition[1];
+        }
+        else if (sceneNum == 3)
+        {
+
+        }
     }
     void Update()
     {
         //collider action
-        if (bubble[0].activeSelf & Input.GetKeyDown(KeyCode.E))  //Graveyaed
+        if (sceneNum == 2)
         {
-            bubble[0].SetActive(false);
-            SceneManager.LoadScene("Scene2_Graveyard");
-        }
-        else if (bubble[1].activeSelf && Input.GetKeyDown(KeyCode.E)) //Forest
-        {
-            bubble[1].SetActive(false);
-            //dialogue
-            //흙과 나무 냄새
-        }
-        else if (bubble[2].activeSelf && Input.GetKeyDown(KeyCode.E))   //Lunohouse
-        {
-            bubble[2].SetActive(false);
-            //SceneManager.LoadScene("Scene5_lunohouse");
-        }
-        else if (bubble[3].activeSelf && Input.GetKeyDown(KeyCode.E))   //Library
-        {
-            bubble[3].SetActive(false);
-            //dialogue
-            //도서관 문은 잠겨있어.
-
-            /*
-            if (!libraryOn)
+            if (bubble[0].activeSelf & Input.GetKeyDown(KeyCode.E))  //Graveyard
             {
-                if (dialogueManager[3].GetDone())
-                    dialogueManager[3].ResetOrder();
-                dialogueManager[3].StartDialogue();
+                bubble[0].SetActive(false);
+                SceneManager.LoadScene("Scene2_Graveyard");
             }
-            else if (libraryOn && hasItemName != "key")  //get key
+            else if (bubble[1].activeSelf && Input.GetKeyDown(KeyCode.E)) //Forest
             {
-                oldDog.SetActive(false);
-                enterLibrary = true;
-                dialogueManager[4].StartDialogue();
-
-                libraryDetail.SetActive(true);
-                player.GetComponent<Move>().enabled = false;
-                player.transform.position = libraryPosition;
-
-                //camera move
-                cameraController.targetPosition
-                     = new Vector3(cam.transform.position.x + 2.5f, cam.transform.position.y, cam.transform.position.z);
-                cameraController.smoothTime = 0.38f;
-                cameraController.cameraMove = false;
-                cameraController.moveRight = true;
-                StartCoroutine(DetailAppear());  //after 0.08s, Detail On
-
-                blackScreenAnim.SetTrigger("FadeOut");
-                StartCoroutine(SceneMove());
+                bubble[1].SetActive(false);
+                //S2-2s-1 dialogue
             }
-            else if (hasItemName == "key")   //use key
+            else if (bubble[2].activeSelf && Input.GetKeyDown(KeyCode.E))   //Lunohouse
             {
-                blackScreenAnim.SetTrigger("FadeOut");
-                StartCoroutine(SceneMove());
+                bubble[2].SetActive(false);
+                //SceneManager.LoadScene("Scene5_lunohouse");
             }
-            */
-        }
-        else if (bubble[4].activeSelf && Input.GetKeyDown(KeyCode.E))   //Ladder1
-        {
-            //dialogue
-            //남의 집 사다리에 함부로 올라가는 것은 예의가 아니야.
-        }
-        else if ((bubble[5].activeSelf || bubble[9].activeSelf)
-            && Input.GetKeyDown(KeyCode.E))   //House1, 2
-        {
-            //dialogue
-            //남의 집에 함부로 들어가는 것은 예의가 아니야.
-        }
-        else if ((bubble[6].activeSelf || bubble[8].activeSelf)
+            else if (bubble[3].activeSelf && Input.GetKeyDown(KeyCode.E))   //Library
+            {
+                bubble[3].SetActive(false);
+                //S2-2s-2 dialogue
+            }
+            else if (bubble[4].activeSelf && Input.GetKeyDown(KeyCode.E))   //Ladder1
+            {
+                //S2-2s-3 dialogue
+            }
+            else if (bubble[5].activeSelf && Input.GetKeyDown(KeyCode.E))   //House1
+            {
+                //S2-2s-4 dialogue
+            }
+            else if ((bubble[6].activeSelf || bubble[8].activeSelf)
             && Input.GetKeyDown(KeyCode.E))   //Bench1, 2
-        {
-            //dialogue
-            //평범한 벤치야.
-        }
-        else if (bubble[7].activeSelf && Input.GetKeyDown(KeyCode.E))   //Fountain
-        {
-            //dialogue
-            //우리 마을 중앙에 있는 분수야.
+            {
+                if (benchDialogue == 0)
+                {
+                    //S2-2s-5 dialogue
+                    benchDialogue++;
+                }
+                else
+                {
+                    //S2-2s-6 dialogue
+                    benchDialogue--;
+                }
+            }
+            else if (bubble[7].activeSelf && Input.GetKeyDown(KeyCode.E))   //Fountain
+            {
+                //S2-2s-7 dialogue
+            }
+            else if (bubble[9].activeSelf && Input.GetKeyDown(KeyCode.E))   //House2
+            {
+                //S2-2s-9
+            }
+
+            //bubble[10]. babybear dialogue S2-2s-8
+
+            else if (bubble[11].activeSelf && Input.GetKeyDown(KeyCode.E))   //Otaku
+            {
+                //S2-2s-10 dialogue
+            }
+            else if (bubble[12].activeSelf && Input.GetKeyDown(KeyCode.E))  //Dog
+            {
+                //S2-2s-11 dialogue
+            }
+
+            //no move for 5 secnods
+            noMoveTime += Time.deltaTime;
+
+            if (Input.anyKeyDown)
+            {
+                noMoveTime = 0f;
+            }
+
+            if (noMoveTime >= 5f)
+            {
+                graveyardBubble.SetActive(true);
+                noMoveTime = 0f;
+            }
+
+            if (graveyardBubble.activeSelf && Input.GetKeyDown(KeyCode.E))
+            {
+                graveyardBubble.SetActive(false);
+            }
         }
 
-        else if (bubble[10].activeSelf && Input.GetKeyDown(KeyCode.E))   //Babybear
+        else if (sceneNum == 3)
         {
-            //animation
-            //dialogue
+            if (bubble[0].activeSelf & Input.GetKeyDown(KeyCode.E))  //Graveyard
+            {
+                bubble[0].SetActive(false);
+                SceneManager.LoadScene("Scene2_Graveyard");
+            }
+            else if (bubble[1].activeSelf && Input.GetKeyDown(KeyCode.E)) //Forest
+            {
+                bubble[1].SetActive(false);
+                //S3-2s-1 dialogue
+            }
+            else if (bubble[2].activeSelf && Input.GetKeyDown(KeyCode.E))   //Lunohouse
+            {
+                bubble[2].SetActive(false);
+                //SceneManager.LoadScene("Scene5_lunohouse");
+            }
+            else if (bubble[3].activeSelf && Input.GetKeyDown(KeyCode.E))   //Library
+            {
+                bubble[3].SetActive(false);
+                //S3-2s-2 dialogue
+            }
+            else if (bubble[4].activeSelf && Input.GetKeyDown(KeyCode.E))   //Ladder1
+            {
+                //S3-2s-3 dialogue
+            }
+            else if (bubble[5].activeSelf && Input.GetKeyDown(KeyCode.E))   //House1
+            {
+                //S3-2s-4 dialogue
+            }
+            else if ((bubble[6].activeSelf || bubble[8].activeSelf)
+            && Input.GetKeyDown(KeyCode.E))   //Bench1, 2
+            {
+                //S3-2s-5 dialogue
+            }
+            else if (bubble[7].activeSelf && Input.GetKeyDown(KeyCode.E))   //Fountain
+            {
+                //S3-2s-6 dialogue
+            }
+            else if (bubble[9].activeSelf && Input.GetKeyDown(KeyCode.E))    //House2
+            {
+                //S3-2s-8 dialogue
+            }
+            else if (bubble[10].activeSelf && Input.GetKeyDown(KeyCode.E))   //babyBear
+            {
+                //S3-2s-7 dialogue
+            }
+            else if (bubble[11].activeSelf && Input.GetKeyDown(KeyCode.E))   //Otaku
+            {
+                //S3-2s-9~15 dialogue
+            }
+            else if (bubble[12].activeSelf && Input.GetKeyDown(KeyCode.E))  //Dog
+            {
+                //S3-2s-16 dialogue
+            }
+
+            //no move for 5 secnods
+            noMoveTime += Time.deltaTime;
+
+            if (Input.anyKeyDown)
+            {
+                noMoveTime = 0f;
+            }
+
+            if (noMoveTime >= 5f)
+            {
+                flowerBubble.SetActive(true);
+                noMoveTime = 0f;
+            }
+
+            if (flowerBubble.activeSelf && Input.GetKeyDown(KeyCode.E))
+            {
+                flowerBubble.SetActive(false);
+            }
         }
 
-        else if (bubble[11].activeSelf && Input.GetKeyDown(KeyCode.E))   //Otaku
+        else if (sceneNum == 4)
         {
-            //dialogue
-        }
+            if (bubble[0].activeSelf & Input.GetKeyDown(KeyCode.E))  //Graveyard
+            {
+                bubble[0].SetActive(false);
+                SceneManager.LoadScene("Scene2_Graveyard");
+            }
+            else if (bubble[3].activeSelf && Input.GetKeyDown(KeyCode.E))   //Library
+            {
+                bubble[3].SetActive(false);
+                //sound
+            }
+            else if (bubble[7].activeSelf && Input.GetKeyDown(KeyCode.E))   //Fountain
+            {
+                //dialogue
+            }
+            else if (bubble[11].activeSelf && Input.GetKeyDown(KeyCode.E))   //Otaku
+            {
 
-        else if (bubble[12].activeSelf && Input.GetKeyDown(KeyCode.E))  //Dog
+            }
+            else if (bubble[12].activeSelf && Input.GetKeyDown(KeyCode.E))  //Dog
+            {
+
+            }
+        }
+        else if (sceneNum == 5)
         {
-            //dialogue
-        }
+            libraryDetail.SetActive(true);
+            player.GetComponent<Move>().enabled = false;
+            player.transform.position = libraryPosition;
 
-        else if (bubble[13].activeSelf && Input.GetKeyDown(KeyCode.E))  //OldDog
+            //camera move
+            cameraController.targetPosition
+                = new Vector3(cam.transform.position.x + 2.5f, cam.transform.position.y, cam.transform.position.z);
+            cameraController.smoothTime = 0.38f;
+            cameraController.cameraMove = false;
+            cameraController.moveRight = true;
+            StartCoroutine(DetailAppear());  //after 0.08s, Detail On
+
+            animator.SetTrigger("FadeOut");
+            StartCoroutine(SceneMove());
+
+            animator.SetTrigger("FadeOut");
+            StartCoroutine(SceneMove());
+
+            //library enter action
+            if (libraryDetail.activeSelf && Input.GetKeyDown(KeyCode.E))
+            {
+                libraryDetail.SetActive(false);
+                noKeyDetail.SetActive(true);
+                //화면 중앙 하단에 ‘Q 닫기’ 안내 메시지 출력. 5초간 노출 후 사라짐. 
+            }
+
+            if (noKeyDetail.activeSelf && Input.GetKeyDown(KeyCode.Q))
+            {
+                animator = noKeyDetail.GetComponent<Animator>();
+                animator.SetTrigger("PressE");
+                StartCoroutine(DetailDisappear());
+            }
+        }
+        else if(sceneNum == 7)
         {
+            if (bubble[13].activeSelf && Input.GetKeyDown(KeyCode.E))  //Dog
+            {
 
+            }
         }
-
-        /*
-        //library enter action
-        if (libraryDetail.activeSelf && Input.GetKeyDown(KeyCode.E))
-        {
-            libraryDetail.SetActive(false);
-            noKeyDetail.SetActive(true);
-            //화면 중앙 하단에 ‘Q 닫기’ 안내 메시지 출력. 5초간 노출 후 사라짐. 
-        }
-
-        if (noKeyDetail.activeSelf && Input.GetKeyDown(KeyCode.Q))
-        {
-            detailAnim = noKeyDetail.GetComponent<Animator>();
-            detailAnim.SetTrigger("PressE");
-            StartCoroutine(DetailDisappear());
-        }
-        */
     }
-
     IEnumerator DetailAppear()
     {
         yield return new WaitForSecondsRealtime(0.08f);
@@ -198,5 +331,13 @@ public class Scene3_SquareManager : MonoBehaviour
     {
         yield return new WaitForSecondsRealtime(1f);
         SceneManager.LoadScene("Scene4_Library");
+    }
+
+    IEnumerator SceneFadeIn()
+    {
+        player.GetComponent<Move>().enabled = false;
+        yield return new WaitForSeconds(1.0f);
+        player.GetComponent<Move>().enabled = true;
+        blackScreen.SetActive(false);
     }
 }
--- LUNO/Assets/Scripts/Scene3_Square/VillageInteraction.cs
+++ LUNO/Assets/Scripts/Scene3_Square/VillageInteraction.cs
@@ -18,99 +18,287 @@ public class VillageInteraction : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        switch(other.gameObject.name)
+        if (UIManager.sceneNum == 2)
         {
-            case "Graveyard":
-                UIManager.bubble[0].SetActive(true);
-                break;
-            case "Forest":
-                UIManager.bubble[1].SetActive(true);
-                break;
-            case "Lunohouse":
-                UIManager.bubble[2].SetActive(true);
-                break;
-            case "Library":
-                UIManager.bubble[3].SetActive(true);
-                break;
-            case "Ladder1":
-                UIManager.bubble[4].SetActive(true);
-                break;
-            case "House1":
-                UIManager.bubble[5].SetActive(true);
-                break;
-            case "Bench1":
-                UIManager.bubble[6].SetActive(true);
-                break;
-            case "Fountain":
-                UIManager.bubble[7].SetActive(true);
-                break;
-            case "Bench2":
-                UIManager.bubble[8].SetActive(true);
-                break;
-            case "House2":
-                UIManager.bubble[9].SetActive(true);
-                break;
-            case "BabyBear":
-                UIManager.bubble[10].SetActive(true);
-                break;
-            case "Otaku":
-                UIManager.bubble[11].SetActive(true);
-                break;
-            case "Dog":
-                UIManager.bubble[12].SetActive(true);
-                break;
-            case "OldDog":
-                UIManager.bubble[13].SetActive(true);
-                break;
+            switch (other.gameObject.name)
+            {
+                case "Graveyard":
+                    UIManager.bubble[0].SetActive(true);
+                    break;
+                case "Forest":
+                    UIManager.bubble[1].SetActive(true);
+                    break;
+                case "Lunohouse":
+                    UIManager.bubble[2].SetActive(true);
+                    break;
+                case "Library":
+                    UIManager.bubble[3].SetActive(true);
+                    break;
+                case "Ladder1":
+                    UIManager.bubble[4].SetActive(true);
+                    break;
+                case "House1":
+                    UIManager.bubble[5].SetActive(true);
+                    break;
+                case "Bench1":
+                    UIManager.bubble[6].SetActive(true);
+                    break;
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(true);
+                    break;
+                case "Bench2":
+                    UIManager.bubble[8].SetActive(true);
+                    break;
+                case "House2":
+                    UIManager.bubble[9].SetActive(true);
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(true);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(true);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 3)
+        {
+            switch (other.gameObject.name)
+            {
+                case "Graveyard":
+                    UIManager.bubble[0].SetActive(true);
+                    break;
+                case "Forest":
+                    UIManager.bubble[1].SetActive(true);
+                    break;
+                case "Lunohouse":
+                    UIManager.bubble[2].SetActive(true);
+                    break;
+                case "Library":
+                    UIManager.bubble[3].SetActive(true);
+                    break;
+                case "Ladder1":
+                    UIManager.bubble[4].SetActive(true);
+                    break;
+                case "House1":
+                    UIManager.bubble[5].SetActive(true);
+                    break;
+                case "Bench1":
+                    UIManager.bubble[6].SetActive(true);
+                    break;
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(true);
+                    break;
+                case "Bench2":
+                    UIManager.bubble[8].SetActive(true);
+                    break;
+                case "House2":
+                    UIManager.bubble[9].SetActive(true);
+                    break;
+                case "BabyBear":
+                    UIManager.bubble[10].SetActive(true);
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(true);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(true);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 4)
+        {
+            switch (other.gameObject.name)
+            {
+                case "Graveyard":
+                    UIManager.bubble[0].SetActive(true);
+                    break;
+                case "Library":
+                    UIManager.bubble[3].SetActive(true);
+                    break;
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(true);
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(true);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(true);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 5)
+        {
+            switch (other.gameObject.name)
+            {
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(true);
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(true);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(true);
+                    break;
+                case "OldDog":
+                    UIManager.bubble[13].SetActive(true);
+                    break;
+
+            }
+        }
+        else if (UIManager.sceneNum == 7)
+        {
+            switch (other.gameObject.name)
+            {
+                case "OldDog":
+                    UIManager.bubble[13].SetActive(true);
+                    break;
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        switch (other.gameObject.name)
+        if (UIManager.sceneNum == 2)
         {
-            case "Graveyard":
-                UIManager.bubble[0].SetActive(false);
-                break;
-            case "Forest":
-                UIManager.bubble[1].SetActive(false);
-                break;
-            case "Lunohouse":
-                UIManager.bubble[2].SetActive(false);
-                break;
-            case "Library":
-                UIManager.bubble[3].SetActive(false);
-                break;
-            case "Ladder1":
-                UIManager.bubble[4].SetActive(false);
-                break;
-            case "House1":
-                UIManager.bubble[5].SetActive(false);
-                break;
-            case "Bench1":
-                UIManager.bubble[6].SetActive(false);
-                break;
-            case "Fountain":
-                UIManager.bubble[7].SetActive(false);
-                break;
-            case "Bench2":
-                UIManager.bubble[8].SetActive(false);
-                break;
-            case "House2":
-                UIManager.bubble[9].SetActive(false);
-                break;
-            case "BabyBear":
-                UIManager.bubble[10].SetActive(false);
-                break;
-            case "Otaku":
-                UIManager.bubble[11].SetActive(false);
-                break;
-            case "Dog":
-                UIManager.bubble[12].SetActive(false);
-                break;
-            case "OldDog":
-                UIManager.bubble[13].SetActive(false);
-                break;
+            switch (other.gameObject.name)
+            {
+                case "Graveyard":
+                    UIManager.bubble[0].SetActive(false);
+                    break;
+                case "Forest":
+                    UIManager.bubble[1].SetActive(false);
+                    break;
+                case "Lunohouse":
+                    UIManager.bubble[2].SetActive(false);
+                    break;
+                case "Library":
+                    UIManager.bubble[3].SetActive(false);
+                    break;
+                case "Ladder1":
+                    UIManager.bubble[4].SetActive(false);
+                    break;
+                case "House1":
+                    UIManager.bubble[5].SetActive(false);
+                    break;
+                case "Bench1":
+                    UIManager.bubble[6].SetActive(false);
+                    break;
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(false);
+                    break;
+                case "Bench2":
+                    UIManager.bubble[8].SetActive(false);
+                    break;
+                case "House2":
+                    UIManager.bubble[9].SetActive(false);
+                    break;
+                case "BabyBear":
+                    //dialogue S2-2s-8
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(false);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(false);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 3)
+        {
+            switch (other.gameObject.name)
+            {
+                case "Graveyard":
+                    UIManager.bubble[0].SetActive(false);
+                    break;
+                case "Forest":
+                    UIManager.bubble[1].SetActive(false);
+                    break;
+                case "Lunohouse":
+                    UIManager.bubble[2].SetActive(false);
+                    break;
+                case "Library":
+                    UIManager.bubble[3].SetActive(false);
+                    break;
+                case "Ladder1":
+                    UIManager.bubble[4].SetActive(false);
+                    break;
+                case "House1":
+                    UIManager.bubble[5].SetActive(false);
+                    break;
+                case "Bench1":
+                    UIManager.bubble[6].SetActive(false);
+                    break;
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(false);
+                    break;
+                case "Bench2":
+                    UIManager.bubble[8].SetActive(false);
+                    break;
+                case "House2":
+                    UIManager.bubble[9].SetActive(false);
+                    break;
+                case "BabyBear":
+                    UIManager.bubble[10].SetActive(false);
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(false);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(false);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 4)
+        {
+            switch (other.gameObject.name)
+            {
+                case "Graveyard":
+                    UIManager.bubble[0].SetActive(false);
+                    break;
+                case "Library":
+                    UIManager.bubble[3].SetActive(false);
+                    break;
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(false);
+                    break;
+                case "BabyBear":
+                    //dialogue
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(false);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(false);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 5)
+        {
+            switch (other.gameObject.name)
+            {
+
+                case "Fountain":
+                    UIManager.bubble[7].SetActive(true);
+                    break;
+                case "Otaku":
+                    UIManager.bubble[11].SetActive(false);
+                    break;
+                case "Dog":
+                    UIManager.bubble[12].SetActive(false);
+                    break;
+                case "OldDog":
+                    UIManager.bubble[13].SetActive(false);
+                    break;
+            }
+        }
+        else if (UIManager.sceneNum == 7)
+        {
+            switch (other.gameObject.name)
+            {
+                case "OldDog":
+                    UIManager.bubble[13].SetActive(false);
+                    break;
+            }
         }
     }
 
