e7de99d6daf9e32942886c256f3cf38798d2ea97
YoungEun <y_e_99@ewhain.net>
2023-01-02 20:20:07 +0900

    add: dialogues

--- LUNO/Assets/Scripts/LunoHouse/LunoHouseManager.cs
+++ LUNO/Assets/Scripts/LunoHouse/LunoHouseManager.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class LunoHouseManager : MonoBehaviour
 {
+    [SerializeField]
+    bool firstPlay;
+
+    //dialogues
+    public GameObject S2_1s;
+    public GameObject S3_3s;
+    public GameObject dayhouse;
+    DialogueManager dialogueManager;
+    bool firstDialogue;
+
     void Start()
     {
-        
+        if(firstPlay)
+        {
+            dialogueManager 
+                = S2_1s.transform.GetChild(1).GetComponent<DialogueManager>();
+            dialogueManager.StartDialogue();
+        }
+        firstDialogue = true;
     }
 
     void Update()
     {
-        
+        if(firstDialogue)
+        {
+
+        }
     }
 }
